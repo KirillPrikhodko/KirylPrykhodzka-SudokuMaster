eb0d67f21539e9f281d3dbca73ba220ccab28ce3
Kiril-Prihodzka <Kiril.Prihodzka@coxautoinc.com>
2021-07-11 21:20:36 +0300

    configure project

--- sudoku-master/src/components/SudokuField/SudokuField.tsx
+++ sudoku-master/src/components/SudokuField/SudokuField.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const SudokuField = () => {
-    return (
-        <div>
-            PRIVET
-        </div>
-    )
-}
+  return <div>PRIVET</div>;
+};
 
 export default SudokuField;
--- sudoku-master/src/pages/SudokuMasterPage.tsx
+++ sudoku-master/src/pages/SudokuMasterPage.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import SudokuField from '../components/SudokuField/SudokuField';
-import './SudokuMasterPage.css';
+import React from "react";
+import SudokuField from "../components/SudokuField/SudokuField";
+import "./SudokuMasterPage.css";
 
 const SudokuMasterPage = () => {
-    return (
-        <div className="sudoku-master-page">
-            <SudokuField />
-        </div>
-    )
-}
+  return (
+    <div className="sudoku-master-page">
+      <SudokuField />
+    </div>
+  );
+};
 
 export default SudokuMasterPage;
